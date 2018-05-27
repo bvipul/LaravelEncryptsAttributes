d1e4a195e6865bf34691d514da29480d4d18108d
Vipul Basapati <basapativipulkumar@gmail.com>
2018-05-27 13:57:14 +0530

    Added exception for decryption errors

--- src/Traits/EncryptsAttributes.php
+++ src/Traits/EncryptsAttributes.php
@@ -2,9 +2,11 @@
 
 namespace Bvipul\EncryptsAttributes\Traits;
 
+use Illuminate\Contracts\Encryption\DecryptException;
+
 trait EncryptsAttributes
 {
-	// Since, Laravel uses env('APP_KEY') for it's internal processes, and also
+    // Since, Laravel uses env('APP_KEY') for it's internal processes, and also
     // the encrypt/decrypt functions. Hence, we need to make sure, we use the
     // same key to encrypt or decrypt other wise we'll get an error. You
     // need to make sure, every developer in your team has the same
@@ -15,7 +17,7 @@ trait EncryptsAttributes
 
         // This event is called when a new record is getting created or updated
         static::saving(function($model) {
-        	$attributes = $model->getAttributes();
+            $attributes = $model->getAttributes();
 
             foreach($attributes as $key => $attribute) {
                 $model->$key = encrypt($attribute);
@@ -24,10 +26,14 @@ trait EncryptsAttributes
 
         // This event is called when we retrieve any record from the database
         static::retrieved(function($model) {
-        	$attributes = $model->getAttributes();
+            $attributes = $model->getAttributes();
             
             foreach($attributes as $key => $attribute) {
-                $model->$key = decrypt($attribute);
+                try {
+                    $model->$key = decrypt($attribute);
+                } catch (DecryptException $e) {
+                    $model->$key = $attribute;
+                }
             }
         });
 
@@ -38,21 +44,21 @@ trait EncryptsAttributes
      */
     public function getAttributes()
     {
-    	$attributes = config('encAttr.useFillableAttributes') 
-    			? $this->getFillableAttributes()
-    			: $this->attributes;
+        $attributes = config('encAttr.useFillableAttributes') 
+                ? $this->getFillableAttributes()
+                : $this->attributes;
 
-    	return $attributes;
+        return $attributes;
     }
 
     /**
      * Get only fillable attributes
      */
-	private function getFillableAttributes()
+    private function getFillableAttributes()
     {
-    	$model = $this;
+        $model = $this;
 
-    	$attributes = collect($model->attributes)->filter(
+        $attributes = collect($model->attributes)->filter(
             function($attribute, $key) use ($model) {
                 return in_array($key, $model->fillable);
             }
